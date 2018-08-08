405254b137725cdbd30de7111330250b7489ae87
Luis <eilstdq@gmail.com>
2018-08-07 22:17:29 -0400

    key and bucket from PUT event, data frame loaded

--- summary.py
+++ summary.py
@@ -2,14 +2,18 @@ import pandas as pd
 import boto3
 from botocore.exceptions import ClientError
 
-def get(event, context):
+s3 = boto3.resource('s3')
 
-    s3 = boto3.resource('s3')
+def create_summary(event, context):
+    file_key = event['Records'][0]['s3']['object']['key']
+    bucket = event['Records'][0]['s3']['bucket']['name']
 
-    tax_percentage = 0.14975
-    #TODO: Change key from s3 PUT event
-    df = pd.read_csv('e-statement-2018-07-14.csv')
+    # get the file so we can use data frames
+    obj = s3.Object(bucket, file_key).download_file('/tmp/working_file.csv')  
+    df = pd.read_csv('/tmp/working_file.csv')
     
+
+    tax_percentage = 0.14975  
     #Sum of columns
     sub_total_sales = df[["'Sale'"]].sum()[0]
     taxes = df [["'Tax'"]].sum()[0]
@@ -18,10 +22,10 @@ def get(event, context):
 
     tax_uber_fee = uber_fee*tax_percentage
 
-    print("Subtotal: " + str(sub_total_sales))
-    print("Taxes: "    + str(taxes))
-    print("Total: "    + str(total))
-    print("Fee: "      + str(uber_fee))
+    print("Subtotal:    " + str(sub_total_sales))
+    print("Taxes:       " + str(taxes))
+    print("Total:       " + str(total))
+    print("Fee:         " + str(uber_fee))
     print("Service tax: "+ str(tax_uber_fee))
 
     # Check that file exists and get a StreamingBody if it does exist
@@ -29,7 +33,7 @@ def get(event, context):
     header = []
     
     try:
-        obj = s3.Object('reportesarriba', 'uberEatsData/hello.csv').get()['Body']
+        obj = s3.Object(BUCKET_NAME, FILE).get()['Body']
         print('Exists')
         char = ''
         str = ''
